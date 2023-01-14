caddc1c048576f25d431eee478acdfc9f97e038f
faseeha.cp <faseeha.cp@teamta.net>
2023-01-14 18:07:58 +0530

    Updates

---
+++ precache-manifest.17465ab03608a6f55291a7a1d2fa00bb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c5202c1610eed2ca2d2a9560eff4a56",
+    "url": "/food-recipe-app/index.html"
+  },
+  {
+    "revision": "574ac00dca7043261afa",
+    "url": "/food-recipe-app/static/css/main.aff11442.chunk.css"
+  },
+  {
+    "revision": "2910ba4615731246b336",
+    "url": "/food-recipe-app/static/js/2.4cd0fae2.chunk.js"
+  },
+  {
+    "revision": "8077996fd3bf7f0950095957af1a9a93",
+    "url": "/food-recipe-app/static/js/2.4cd0fae2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "574ac00dca7043261afa",
+    "url": "/food-recipe-app/static/js/main.00060271.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/food-recipe-app/static/js/main.00060271.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5518bf3fa78573f56fab",
+    "url": "/food-recipe-app/static/js/runtime-main.07d5e000.js"
+  },
+  {
+    "revision": "8b77828236b612850894d82418dbefba",
+    "url": "/food-recipe-app/static/media/profile.8b778282.jpg"
+  }
+]);
